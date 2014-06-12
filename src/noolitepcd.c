6062e8df9159e284b4d4b6c9254d15e916cd9ff8
olegart <oleg@olegart.ru>
2014-06-13 00:43:44 +0400

    tabs to spaces

--- src/noolitepcd.c
+++ src/noolitepcd.c
@@ -13,13 +13,13 @@ int main(int argc, char * argv[])
     unsigned int level;
     unsigned char command[1], buf[8], channel;
     char param;
-	unsigned char channels;
-	FILE *fifo;
-	
-	char input[26];
-	
-	do_exit = 0;
-	static struct sigaction act; 
+    unsigned char channels;
+    FILE *fifo;
+    
+    char input[26];
+    
+    do_exit = 0;
+    static struct sigaction act; 
     sigemptyset (&act.sa_mask);
     act.sa_flags = 0;
     act.sa_handler = SIG_IGN;
@@ -31,19 +31,19 @@ int main(int argc, char * argv[])
     act.sa_handler =  cleanup;
     sigaction (SIGKILL, &act, 0);
 
-	int s, s2, t, len;
+    int s, s2, t, len;
     struct sockaddr_un local, remote;
     if ((s = socket(AF_UNIX, SOCK_STREAM, 0)) == -1)
-	{
-		printf("Socket error\n");
+    {
+        printf("Socket error\n");
         exit(EXIT_FAILURE);
     }
-	local.sun_family = AF_UNIX;
+    local.sun_family = AF_UNIX;
     strcpy(local.sun_path, NSOCKET);
     unlink(local.sun_path);
     len = strlen(local.sun_path) + sizeof(local.sun_family);
     if (bind(s, (struct sockaddr *)&local, len) == -1)
-	{
+    {
         printf("Socket bind failed\n");
         exit(EXIT_FAILURE);
     }
@@ -51,8 +51,8 @@ int main(int argc, char * argv[])
         printf("Socket listen failed\n");
         exit(EXIT_FAILURE);
     }
-	
-	libusb_init(NULL);
+    
+    libusb_init(NULL);
     libusb_set_debug(NULL, 3);
     handle = libusb_open_device_with_vid_pid(NULL, DEV_VID, DEV_PID);
 
@@ -67,7 +67,7 @@ int main(int argc, char * argv[])
     struct libusb_device_descriptor desc;
     libusb_get_device_descriptor(libusb_get_device(handle), &desc);
     libusb_get_string_descriptor_ascii(handle, desc.iProduct, str_desc, 10);
-	channels = atoi(str_desc+4);
+    channels = atoi(str_desc+4);
     
     if (libusb_kernel_driver_active(handle,DEV_INTF))
     {
@@ -88,168 +88,168 @@ int main(int argc, char * argv[])
         exit(EXIT_FAILURE);
     }
 
-	// fork to background
-	if (argc > 1)
+    // fork to background
+    if (argc > 1)
     {
         if (strcmp (argv[1], "-d") == 0)  // run as daemon
         {
             if (daemon(0, 0))
-			{
-				printf("Error forking to background\n");
-				libusb_close(handle);
-				libusb_exit(NULL);
-				exit(EXIT_FAILURE);
-			}
-		}
-	}
-	
+            {
+                printf("Error forking to background\n");
+                libusb_close(handle);
+                libusb_exit(NULL);
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+    
     char pidval[10];
     int pidfile = open(PID_NAME, O_CREAT | O_RDWR, 0666);
     
-	if (lockf(pidfile, F_TLOCK, 0) == -1)
+    if (lockf(pidfile, F_TLOCK, 0) == -1)
     {
         libusb_close(handle);
         libusb_exit(NULL);
-		exit(EXIT_FAILURE);
+        exit(EXIT_FAILURE);
     }
     sprintf(pidval, "%d\n", getpid());
     write(pidfile, pidval, strlen(pidval));
-	
-	while (!do_exit)
+    
+    while (!do_exit)
     {
-	    s2 = accept(s, (struct sockaddr *)&remote, &t);
-		i = recv(s2, input, 25, 0);
-		input[i] = 0; // null-terminated string
+        s2 = accept(s, (struct sockaddr *)&remote, &t);
+        i = recv(s2, input, 25, 0);
+        input[i] = 0; // null-terminated string
         close(s2);
-		
-		char * cmd[5];
-		cmd[0] = strtok(input, " \n");
-		for (i=1; i<5; i++)
-		{
-			cmd[i] = strtok(NULL, " \n");
-		}
-			
-		if (strcmp (cmd[0], "on") == 0)  //Set cnannel ON
-		{
-			COMMAND_ACTION[1] = 2;
-		}
-		else if (strcmp(cmd[0], "off") == 0) //Set channel OFF
-		{
-			COMMAND_ACTION[1] = 0;
-		}
-		else if (strcmp(cmd[0], "switch") == 0) //Switch channel ON/OFF
-		{
-			COMMAND_ACTION[1] = 4;
-		}
-		else if (strcmp(cmd[0], "set") == 0) //Set level on channel - needed arg "level"
-		{
-			COMMAND_ACTION[1] = 6;
-			COMMAND_ACTION[2] = 1; // формат
-			if (cmd[2] != NULL)
-			{
-				level = atoi(cmd[2]);
-			}
-			else
-			{
-				printf("Missing brightness value. \nUsage: %s <command> <channel> [<level>]\n", argv[0]);
-				return -1;
-			}
-			if (level>100)
-			{
-				level=100;
-			}
-			if (level<0)
-			{
-				level=0;
-			}
-			if (level>0)
-			{  
-				level=(int)(34+(float)level*1.23);
-			}
-			COMMAND_ACTION[5]= level;
-		} 
-		else if (strcmp(cmd[0], "bind") == 0) //Привязать канал
-		{
-			COMMAND_ACTION[1] = 15;
-		}
-		else if (strcmp(cmd[0], "unbind") == 0) //отвязать канал
-		{
-			COMMAND_ACTION[1] = 9;
-		}
-		else if (strcmp(cmd[0], "preset") == 0) //Вызов записанного ранее в программе сценария освещения presetX, где X – номер сценария в программе (1…5)
-		{
-			//COMMAND_ACTION[1] = ?; // не реализовано
-		} 
-		else if (strcmp(cmd[0], "load") == 0) //Команда вызова записанного сценария из памяти силового блока для канала X
-		{
-			COMMAND_ACTION[1] = 7;
-		} 
-		else if (strcmp(cmd[0], "save") == 0) //Команда записи сценария в память силового блока для канала X
-		{
-			COMMAND_ACTION[1] = 8;
-		} 
-		else if (strcmp(cmd[0], "stop") == 0) //остановить регулировку 
-		{
-			COMMAND_ACTION[1] = 10;
-		} 
-		else if (strcmp(cmd[0], "color_roll") == 0) //включение плавного перебора цвета, выключается командой 10.
-		{
-			COMMAND_ACTION[1] = 16;
-			COMMAND_ACTION[2] = 4; // формат
-		} 
-		else if (strcmp(cmd[0], "color_switch") == 0) //переключение цвета 
-		{
-			COMMAND_ACTION[1] = 17;
-			COMMAND_ACTION[2] = 4; // формат
-		} 
-		else if (strcmp(cmd[0], "mode") == 0) //переключение режима работы 
-		{
-			COMMAND_ACTION[1] = 18;
-			COMMAND_ACTION[2] = 4; // формат
-		} 
-		else if (strcmp(cmd[0], "mode_speed") == 0) //переключение скорости эффекта в режиме работы
-		{
-			COMMAND_ACTION[1] = 19;
-			COMMAND_ACTION[2] = 4; // формат
-		} 
-		else if (strcmp(cmd[0], "color") == 0) //Установка яркости на каждый канал независимо (R - 1, G - 2, B - 3). Уровень передается параметрами в формате 0…255
-		{ 
-			COMMAND_ACTION[1] = 6;
-			COMMAND_ACTION[2] = 3; // формат
-			COMMAND_ACTION[5] = atoi(cmd[2]); // R
-			COMMAND_ACTION[6] = atoi(cmd[3]); // G
-			COMMAND_ACTION[7] = atoi(cmd[4]); // B
-		}
-		else
-		{
-			continue;
-		}
-			
-		if (cmd[1] != NULL)
-		{
-			channel = atoi(cmd[1]);
-			channel--;
-			COMMAND_ACTION[4] = channel;
-		}
-			
-		//0x9 - номер запроса
-		//0x300 - значение запроса - их надо получить из мониторинга
+        
+        char * cmd[5];
+        cmd[0] = strtok(input, " \n");
+        for (i=1; i<5; i++)
+        {
+            cmd[i] = strtok(NULL, " \n");
+        }
+            
+        if (strcmp (cmd[0], "on") == 0)  //Set cnannel ON
+        {
+            COMMAND_ACTION[1] = 2;
+        }
+        else if (strcmp(cmd[0], "off") == 0) //Set channel OFF
+        {
+            COMMAND_ACTION[1] = 0;
+        }
+        else if (strcmp(cmd[0], "switch") == 0) //Switch channel ON/OFF
+        {
+            COMMAND_ACTION[1] = 4;
+        }
+        else if (strcmp(cmd[0], "set") == 0) //Set level on channel - needed arg "level"
+        {
+            COMMAND_ACTION[1] = 6;
+            COMMAND_ACTION[2] = 1; // формат
+            if (cmd[2] != NULL)
+            {
+                level = atoi(cmd[2]);
+            }
+            else
+            {
+                printf("Missing brightness value. \nUsage: %s <command> <channel> [<level>]\n", argv[0]);
+                return -1;
+            }
+            if (level>100)
+            {
+                level=100;
+            }
+            if (level<0)
+            {
+                level=0;
+            }
+            if (level>0)
+            {  
+                level=(int)(34+(float)level*1.23);
+            }
+            COMMAND_ACTION[5]= level;
+        } 
+        else if (strcmp(cmd[0], "bind") == 0) //Привязать канал
+        {
+            COMMAND_ACTION[1] = 15;
+        }
+        else if (strcmp(cmd[0], "unbind") == 0) //отвязать канал
+        {
+            COMMAND_ACTION[1] = 9;
+        }
+        else if (strcmp(cmd[0], "preset") == 0) //Вызов записанного ранее в программе сценария освещения presetX, где X – номер сценария в программе (1…5)
+        {
+            //COMMAND_ACTION[1] = ?; // не реализовано
+        } 
+        else if (strcmp(cmd[0], "load") == 0) //Команда вызова записанного сценария из памяти силового блока для канала X
+        {
+            COMMAND_ACTION[1] = 7;
+        } 
+        else if (strcmp(cmd[0], "save") == 0) //Команда записи сценария в память силового блока для канала X
+        {
+            COMMAND_ACTION[1] = 8;
+        } 
+        else if (strcmp(cmd[0], "stop") == 0) //остановить регулировку 
+        {
+            COMMAND_ACTION[1] = 10;
+        } 
+        else if (strcmp(cmd[0], "color_roll") == 0) //включение плавного перебора цвета, выключается командой 10.
+        {
+            COMMAND_ACTION[1] = 16;
+            COMMAND_ACTION[2] = 4; // формат
+        } 
+        else if (strcmp(cmd[0], "color_switch") == 0) //переключение цвета 
+        {
+            COMMAND_ACTION[1] = 17;
+            COMMAND_ACTION[2] = 4; // формат
+        } 
+        else if (strcmp(cmd[0], "mode") == 0) //переключение режима работы 
+        {
+            COMMAND_ACTION[1] = 18;
+            COMMAND_ACTION[2] = 4; // формат
+        } 
+        else if (strcmp(cmd[0], "mode_speed") == 0) //переключение скорости эффекта в режиме работы
+        {
+            COMMAND_ACTION[1] = 19;
+            COMMAND_ACTION[2] = 4; // формат
+        } 
+        else if (strcmp(cmd[0], "color") == 0) //Установка яркости на каждый канал независимо (R - 1, G - 2, B - 3). Уровень передается параметрами в формате 0…255
+        { 
+            COMMAND_ACTION[1] = 6;
+            COMMAND_ACTION[2] = 3; // формат
+            COMMAND_ACTION[5] = atoi(cmd[2]); // R
+            COMMAND_ACTION[6] = atoi(cmd[3]); // G
+            COMMAND_ACTION[7] = atoi(cmd[4]); // B
+        }
+        else
+        {
+            continue;
+        }
+            
+        if (cmd[1] != NULL)
+        {
+            channel = atoi(cmd[1]);
+            channel--;
+            COMMAND_ACTION[4] = channel;
+        }
+            
+        //0x9 - номер запроса
+        //0x300 - значение запроса - их надо получить из мониторинга
 
-		if ((ret = libusb_control_transfer(handle, LIBUSB_REQUEST_TYPE_CLASS|LIBUSB_RECIPIENT_INTERFACE|LIBUSB_ENDPOINT_OUT, 0x9, 0x300, 0, COMMAND_ACTION, 8, 100)) < 0)
-		{
-			printf("USB data transfer error %i.\n", ret);
-		}
-		usleep(350000); // 350 ms sleep
-	}
-	
+        if ((ret = libusb_control_transfer(handle, LIBUSB_REQUEST_TYPE_CLASS|LIBUSB_RECIPIENT_INTERFACE|LIBUSB_ENDPOINT_OUT, 0x9, 0x300, 0, COMMAND_ACTION, 8, 100)) < 0)
+        {
+            printf("USB data transfer error %i.\n", ret);
+        }
+        usleep(350000); // 350 ms sleep
+    }
+    
     libusb_attach_kernel_driver(handle, DEV_INTF);
     libusb_close(handle);
     libusb_exit(NULL);
-	
-	lockf(pidfile, F_ULOCK, 0);
-	close(pidfile);
+    
+    lockf(pidfile, F_ULOCK, 0);
+    close(pidfile);
     remove(PID_NAME);
-	
+    
     return 0;
 }
 
