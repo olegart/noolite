ed8957340a0a6c562237a10d435d148e8183913b
olegart <oleg@olegart.ru>
2014-07-26 00:08:18 +0400

    Fix API & command line parsing

--- src/nooliterxcfg.c
+++ src/nooliterxcfg.c
@@ -17,6 +17,18 @@
 
 unsigned char COMMAND_ACTION[8] = {0x30,0x00,0x00,0x00,0x00,0x00,0x00,0x00}; //{80,0x00,0xaa,0x00,0x0,0x1,0x14,0x05}
 
+void usage()
+{
+    printf("Usage: %s <command> [channel]\n", argv[0]);
+    printf("        <command> may be:\n");
+	printf("                --clear - clear previously binded channel\n");
+	printf("                --clearall - clear ALL channels\n");
+    printf("                --bind - bind channel (30 seconds)\n");
+    printf("                --stop - stop binding channel\n");
+	printf("                --help - print help and exit\n");
+    printf("        <channel> must be [1..64]\n");
+}
+
 int main(int argc, char * argv[])
 {
         libusb_device_handle * handle;
@@ -26,94 +38,87 @@ int main(int argc, char * argv[])
         unsigned char command[1], buf[8], channel;
         char param;
         
-        //Arg Control
-
         if (argc == 1) {
-                printf("Usage: %s --help\n", argv[0]);
-                return -1;
+                usage();
+                return 0;
         }
 
         if (strcmp (argv[1],"--help")==0)
         {
-            printf("Usage: %s <command> <channel> [<level>]\n", argv[0]);
-            printf("        <command> may be:\n");
-            printf("                --normal - back to normal mode \n");
-            printf("                --clear - clear \n");
-            printf("                --clearall - Clear ALL\n");
-            printf("                --reset - Reset channel\n");
-            printf("                --bind - Bind channel\n");
-            printf("                --unbind - Unbind channel\n");
-            printf("        <channel> must be [1..64]\n");
-            return -1;
+            usage();
+            return 0;
         }
-        
-        if (argc >= 3)
-        {
-            if (strcmp (argv[1],"--normal")==0) //0-нормальная работа
+		
+		if (argc == 2) // no channel number expected
+		{
+			if ((strcmp(argv[1], "--bind") == 0) || (strcmp(argv[1], "--clear"))
+			{
+				printf("No channel number given.\n");
+				usage();
+				return -1;
+			}
+			
+            if (strcmp(argv[1], "--stop") == 0) // 2 - остановить привязку принудительно
             {
                 COMMAND_ACTION[0] = 2;
             }
-            else if (strcmp(argv[1],"--bind")==0) //1-включить привязку на адрес ячейки, 30 секунд
-            {
-                COMMAND_ACTION[0] = 1;
-            }
-            else if (strcmp(argv[1],"--unbind")==0) //2- выключить привязку принудительно
+            else if (strcmp(argv[1], "--clearall") == 0) // 4 - очистить всю память 
             {
                 COMMAND_ACTION[0] = 4;
             }
-            else if (strcmp(argv[1],"--clear")==0) //3- очистить ячейку (адрес ячейки)
+			else
+			{
+				printf("Unknown command\n");
+				usage();
+				return -1;
+			}
+			
+			COMMAND_ACTION[1] = 0; // no channel number needed for these commands
+		}
+        
+        if (argc >= 3)
+        {
+            if (strcmp(argv[1], "--bind") == 0) // 1 - включить привязку на адрес ячейки, 30 секунд
             {
-                COMMAND_ACTION[0] = 8;
+                COMMAND_ACTION[0] = 1;
             }
-            else if (strcmp(argv[1],"--clearall")==0) //4- очистить всю память 
+            else if (strcmp(argv[1], "--clear") == 0) // 3 - очистить ячейку
             {
-                COMMAND_ACTION[0] = 16;
-            }
-            else if (strcmp(argv[1],"--reset")==0) //5- сбросить значение калибровки USB 
-            {
-                COMMAND_ACTION[0] = 32;
+                COMMAND_ACTION[0] = 3;
             }
             else
             {
                 printf("Unknown command\n");
+				usage();
                 return -1;
             }
-        }
-        else
-        {
-            printf("No command was given. Usage: %s <command> <channel>\n", argv[0]);
-            return -1;
-        }
-
-        if (argc >= 3)
-        {
-            channel = atoi(argv[2]);
+			
+			channel = atoi(argv[2]);
             channel--;
             if ((channel>63)||(channel<0))
             {
-                printf("Channel number out of range (1-64)\nUsage: %s <command> <channel> \n", argv[0]);
+                printf("Channel number out of range (1-64)\n", argv[0]);
+				usage();
                 return -1;
             }
             COMMAND_ACTION[1] = channel;
-        }
-        else
-        {
-            printf("No channel was given.\nUsage: %s <command> <channel>\n", argv[0]);
-            return -1;
         }
 
 //Prepare Command string
         libusb_init(NULL);
         libusb_set_debug(NULL, 3);
         handle = libusb_open_device_with_vid_pid(NULL, DEV_VID, DEV_PID);
-        if (handle == NULL) {
-         printf("No compatible USB devices were found.\n");
-         libusb_exit(NULL);
-         return 0;
+        if (handle == NULL)
+		{
+			printf("No compatible USB devices were found.\n");
+			libusb_exit(NULL);
+			return 0;
         }
         if (libusb_kernel_driver_active(handle,DEV_INTF))
+		{
                 libusb_detach_kernel_driver(handle, DEV_INTF);
-        if ((ret = libusb_set_configuration(handle, DEV_CONFIG)) < 0)
+        }
+		if ((ret = libusb_set_configuration(handle, DEV_CONFIG)) < 0)
         {
                 printf("USB configuration error\n");
                 libusb_close(handle);
