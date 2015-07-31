cac521e494bce8693b3c83729577e73953bde83b
olegart <oleg@olegart.ru>
2015-07-31 13:05:16 +0300

    Misprint

--- src/nooliterx.c
+++ src/nooliterx.c
@@ -227,7 +227,7 @@ int main(int argc, char * argv[])
                 if (customcommand)
                 {
 					char *repstr;
-					char searchfor[8] = ("%st", "%ch", "%sm", "%df", "%d0", "%d1", "%d2", "%d3");
+					char searchfor[8] = {"%st", "%ch", "%sm", "%df", "%d0", "%d1", "%d2", "%d3"};
 					for (int k=0; k<8; k++)
 					{
 						repstr = str_replace(cmd, searchfor[k], int_to_str(buf[k]));
